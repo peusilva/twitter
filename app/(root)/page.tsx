e0d1ec0fd708398c8c04d0a79fb42062469138d1
Pedro Silva <pedro.alsf94@gmail.com>
2023-09-18 13:00:28 -0300

    fetch posts and card start

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import { UserButton } from "@clerk/nextjs";
- 
-export default function Home() {
+import TweetCard from "@/components/cards/TweetCard";
+import { fetchPosts } from "@/lib/actions/tweet.actions";
+import { currentUser } from "@clerk/nextjs";
+
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+  const user = await currentUser();
+
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
+
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No Tweets found</p>
+        ):(
+          <>
+          {result.posts.map((post)=> (
+            <TweetCard
+            key={post._id}
+            id={post._id}
+            currentUserId={user?.id || ""}
+            parentId={post.parentId}
+            content={post.text}
+            author={post.author}
+            community={post.community}
+            createdAt={post.createdAt}
+            comments={post.children}
+            />
+          ))}
+          </>
+        )}
+      </section>
     </div>
   )
 }
--- lib/actions/tweet.actions.ts
+++ lib/actions/tweet.actions.ts
@@ -28,3 +28,33 @@ export async function createTweet({text, author, communityId, path}: Params) {
 
     revalidatePath(path);
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    //Calculate number of posts to skip
+    const skipAmount = (pageNumber -1 ) * pageSize;
+
+    // Fetch the posts that ahave no parents (top-level tweets...)
+    const postsQuery = Tweet.find({ parentId: { $in: [null, undefined]}})
+        .sort ({ createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User})
+        .populate ({
+             path: 'children',
+             populate: {
+                path: 'author',
+                model: User,
+                select: '_id name parentId image'
+             }
+            })
+
+            const totalPostsCount = await Tweet.countDocuments({ parentId: { $in: [null, undefined]}})
+
+            const posts = await postsQuery.exec();
+
+            const isNext = totalPostsCount > skipAmount + posts.length;
+
+            return { posts, isNext }
+}
